e8d914bdfd80dcc986de7bde1b304961a53981ef
Aaron Liu <aaron.j.liu@gmail.com>
2022-06-20 18:45:29 -0400

    Added form validation with error messages... Checks whether inputs have inputs Checks whether email is valid Checks whether phone number is valid Checks whether both passwords are same Changed default link color to match parent Added another logo image to the right of title

---
+++ script.js
@@ -0,0 +1,63 @@
+
+function passwordValidity(event) {
+    console.log("executed");
+    const password = document.getElementById('user-password');
+    const confirmPassword = document.getElementById('confirm-password');
+    for(let i=0; i<inputs.length; i++) {
+        const errorMessage = document.querySelector('#' + inputs[i].id + ' + .error');
+        if(inputs[i].value.length == 0) {
+            inputs[i].setCustomValidity("Error");
+            errorMessage.textContent = "*Please fill out this field.";
+            event.preventDefault();
+        }
+        else {
+            inputs[i].setCustomValidity("");
+            errorMessage.textContent = "";
+        }
+    }
+    
+    const email = document.getElementById('user-email');
+    if(!email.validity.valid) {
+        document.querySelector('#user-email + .error').textContent = "*Invalid email";
+    }
+
+    const phoneNumber = document.getElementById('phone');
+    if(!email.validity.valid) {
+        document.querySelector('#phone + .error').textContent = "*Invalid phone number.";
+    }
+
+    if(password.value.length == 0) {
+        return;
+    }
+    else if(password.value != confirmPassword.value) {
+        password.setCustomValidity("Error");
+        confirmPassword.setCustomValidity("Error");
+        document.querySelector('#user-password + .error').textContent = "*Please put in same passwords.";
+        event.preventDefault();
+    }
+    else {
+        password.setCustomValidity("");
+        confirmPassword.setCustomValidity("");
+        document.querySelector('#user-password + .error').textContent = "";
+    }
+}
+
+function updateValidity(event) {
+    const errorMessage = document.querySelector('#' + event.target.id + ' + .error');
+    if(event.target.value.length == 0) {
+        event.target.setCustomValidity("Error");
+        errorMessage.textContent = "*Please fill out this field.";
+    }
+    else {
+        event.target.setCustomValidity("");
+        errorMessage.textContent = "";
+    }
+}
+
+const inputs = document.querySelectorAll('input');
+const form = document.getElementById('sushi-form');
+form.addEventListener('submit', passwordValidity);
+inputs.forEach((input) => {
+    input.addEventListener('input', updateValidity);
+})
+
